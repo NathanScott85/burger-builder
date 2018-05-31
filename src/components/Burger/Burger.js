06366e635bd7659ec180ba333f0715df452183d8
NathanScott85 <nathansfeely@gmail.com>
2018-05-31 20:30:40 +0100

    Calculating Ingredient  Sum

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import styles from './Burger.css';
-import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
+import React from "react";
+import styles from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 
-const burger = (props) => {
+const burger = props => {
+  let transformedIngredients = Object.keys( props.ingredients )
+  .map( igKey => {
+      return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+          return <BurgerIngredient key={igKey + i} type={igKey} />;
+      } );
+  } )
+  .reduce((arr, el) => {
+      return arr.concat(el)
+  }, []);
 
-    const transformedIngredients = Object.keys( props.ingredients )
-    .map( igKey => {
-        return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
-            return <BurgerIngredient key={igKey + i} type={igKey} />;
-        });
-    });
-
-    return(
-        <div className={styles.Burger}>        
-        <BurgerIngredient type="bread-top"/>
-        {transformedIngredients}
-        <BurgerIngredient type="bread-bottom"/>
-         </div>
- 
-    );
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please Add Ingredients</p>;
+  }
+  return (
+    <div className={styles.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
 };
 
 export default burger;
