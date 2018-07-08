f9eb3eb39f193c4f44e8b201f95256ab5bdcba70
NathanScott85 <nathansfeely@gmail.com>
2018-07-08 16:48:21 +0100

    add, remove & disabled

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -15,7 +15,10 @@ const buildControls = (props) => (
         {controls.map(ctrl => (
             <BuildControl 
                 key={ctrl.label} 
-                label={ctrl.label} />
+                label={ctrl.label} 
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                added={() => props.ingredientAdded(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}/>
         ))}
     </div>
 );  
