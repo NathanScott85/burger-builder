321f69924efe081c3e75803c17aad4c2e65aa10e
NathanScott85 <nathansfeely@gmail.com>
2018-07-09 14:05:19 +0100

    Fixed NaN Issue causing incorrect Calculation

--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../Burger/BuildControls/BuildControls';
 
 const INGREDIENT_PRICES = {
-      salad: 0.5,
-      cheese: 0.4,
-      meat: 0.9,
-      bacon: 0.6
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 0.9,
+    bacon: 0.6
 };
 
 class BurgerBuilder extends Component {
@@ -21,51 +21,53 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4
     }
-    addIngredientHandler = (type) => {
+    addIngredientHandler = ( type ) => {
         const oldCount = this.state.ingredients[type];
-        const updatedCount = oldCount +1;
+        const updatedCount = oldCount + 1;
         const updatedIngredients = {
             ...this.state.ingredients
         };
         updatedIngredients[type] = updatedCount;
         const priceAddition = INGREDIENT_PRICES[type];
-        const oldPrice = this.setState.totalPrice;
+        const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
     }
-    removeIngredientHandler = (type) =>{
+    removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
-        if(oldCount <= 0){
+        if (oldCount <= 0) {
             return;
         }
-        const updatedCount = oldCount -1;
+        const updatedCount = oldCount - 1;
         const updatedIngredients = {
             ...this.state.ingredients
         };
         updatedIngredients[type] = updatedCount;
         const priceDeduction = INGREDIENT_PRICES[type];
-        const oldPrice = this.setState.totalPrice;
+        const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
     }
     render() {
-        const disabledInfo   = {
+        const disabledInfo = {
             ...this.state.ingredients
         }
         for (let key in disabledInfo) {
-            disabledInfo[key] = disabledInfo[key] <=0
+            disabledInfo[key] = disabledInfo[key] <= 0
         }
         return (
             <Aux>
-             <Burger ingredients={this.state.ingredients}/> 
-             {/* <p> Build Controls</p> */}
-            <BuildControls 
-            ingredientAdded={this.addIngredientHandler}
-            ingredientRemoved={this.removeIngredientHandler}
-            disabled={disabledInfo}/>
+                <Burger ingredients={this.state.ingredients} />
+                {/* <p> Build Controls</p> */}
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                             price={this.state.totalPrice} 
+                    />
             </Aux>
         );
-    } 
+    }
 }
 
 export default BurgerBuilder;
